6510f0c10694f7396b7940230cadbaecf3172b67
Shakhid34 <104143190+Shakhid34@users.noreply.github.com>
2022-04-21 18:59:58 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1650542365,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tiledbackground3.png",
+		"images/sprite4-sheet0.png",
+		"images/i__4_removebgpreview-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
